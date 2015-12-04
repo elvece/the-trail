7948e1cd58c63309c0c1bc96d06215afdbafa188
elvece <lvcifferello@gmail.com>
2015-12-03 21:39:47 -0700

    removed nav controller and added hike directory

--- src/client/public/nav/nav-directive.js
+++ src/client/public/nav/nav-directive.js
@@ -2,9 +2,7 @@ angular.module('directives')
   .directive('navTemplate', [function(){
     return {
       restrict: 'A',
-      templateUrl: 'nav/nav.html',
-      controller: function($scope, authFactory){
-      }
+      templateUrl: 'nav/nav.html'
     };
   }]);
 
