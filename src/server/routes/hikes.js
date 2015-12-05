5d3bd7702720e48db7a0cc01ea72fcc75b1ee398
elvece <lvcifferello@gmail.com>
2015-12-05 14:50:19 -0700

    get all and get single hike routes working

--- src/server/routes/hikes.js
+++ src/server/routes/hikes.js
@@ -7,12 +7,21 @@ var Hike = mongoose.model('hike');
 router.get('/all', function(req, res, next){
   Hike.find(function(err, hikes){
     if(err){
-      console.log(err);
       res.json(err);
     }
     else{
-      console.log(hikes);
       res.json(hikes);
+    }
+  });
+});
+
+router.get('/hike/:id', function(req, res, next) {
+  Hike.findById(req.params.id, function(err, data){
+    if(err){
+      res.json(err);
+    }
+    else{
+      res.json(data);
     }
   });
 });
