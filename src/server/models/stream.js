bc44c2720e37bf72ffaa18c30e4f7a194316d4bb
elvece <lvcifferello@gmail.com>
2015-12-05 14:24:04 -0700

    set up stream schema

--- src/server/models/stream.js
+++ src/server/models/stream.js
@@ -1,3 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+//entire stream
+var Stream = new Schema({
+  room: String,
+  users: Array,
+  messages: [{type: Schema.Types.ObjectId,
+    ref:'chatMessages'}]
+});
+
+//individual comments
+var Comment = new Schema({
+  user: String,
+  message: String
+});
+
+
+module.exports = mongoose.model('comment', Comment);
+module.exports = mongoose.model('stream', Stream);
