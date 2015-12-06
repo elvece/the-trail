bb0d32a00600d1955332e36f0e84afc4f7aae160
elvece <lvcifferello@gmail.com>
2015-12-06 11:29:53 -0700

    styled stream box and set up back end socket connections

--- src/client/public/geo/stream-directive.js
+++ src/client/public/geo/stream-directive.js
@@ -6,10 +6,22 @@ angular.module('directives')
       controller: function($scope, streamFactory){
         //leave localhost direct connection for local development, take out for heroku
         var socket = io.connect('http://localhost:3000');
-        socket.on('hi', function(data){
-          console.log(data);
-          //then can emit another event
+        var streamBoard = angular.element(document.querySelector('#chat-ul'));
+        var onlineUsers = angular.element(document.querySelector('#user-ul'));
+
+        // socket.on('hi', function(data){
+        //   console.log(data);
+        //   //then can emit another event
+        // });
+
+        socket.on('entered', function(data){
+          //display current users
         });
+
+
+        function displayStream(){
+
+        }
       }
     };
   }]);
--- src/server/socket.js
+++ src/server/socket.js
@@ -2,7 +2,25 @@ module.exports = function(io) {
   io.on('connection', function(socket) {
     console.log('connection activated!');
 
-    socket.emit('hi', { hello: 'world' });
+    // socket.emit('hi', { hello: 'world' });
+    var users = [];
+    //current users in room
+    socket.on('entered', function(user){
+      if(users.indexOf(user.username) === -1){
+        users.push(user.username);
+      }
+      socket.user = user.username;
+      io.to(socket.room).emit('current-users', users);
+    });
+
+    //message to stream
+    socket.on('comment-sent', function(message){
+      io.to(socket.room).emit('comment-received', {
+        message: message,
+        user: socket.user,
+        location: location
+      });
+    });
 
   });
 };
