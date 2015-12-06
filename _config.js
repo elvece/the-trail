653e6702ecf24723b075c2ac62d1300261405e5a
elvece <lvcifferello@gmail.com>
2015-12-05 19:57:33 -0700

    added favicon, trying to fix nav bar glitch, launched to heroku

--- _config.js
+++ _config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongoURI: {
   development: 'mongodb://localhost/the-trail',
-  production: ''//heroku
+  production: 'mongodb://heroku_46h25fvb:nnlfk2f9geo6672jf14490bs87@ds061268.mongolab.com:61268/heroku_46h25fvb'//heroku
   },
   accountSid: '',//twillio
 };
