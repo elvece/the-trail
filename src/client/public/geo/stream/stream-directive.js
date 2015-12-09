c94ce63330a16443780bba24489e9fdb660488b6
elvece <lvcifferello@gmail.com>
2015-12-09 06:50:02 -0700

    trying for heroku

--- src/client/public/geo/stream/stream-directive.js
+++ src/client/public/geo/stream/stream-directive.js
@@ -8,7 +8,7 @@ angular.module('directives')
         ///// *** GLOBALS *** /////
 
         //for local: 'http://localhost:3000', remove for heroku
-        var socket = io.connect();
+        var socket = io.connect('//the-trail.herokuapp.com/#/');
         //to populate view
         var streamBoard = angular.element(document.querySelector('#stream-board'));
         var mapDiv = angular.element(document.querySelector('#map'));
